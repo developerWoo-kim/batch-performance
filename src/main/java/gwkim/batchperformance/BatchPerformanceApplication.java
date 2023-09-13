b918b4cb986a82461a709b1b2c105533aa48a2a1
gunwo151@gmail.com <gwkim@d-pub.co.kr>
2023-09-13 22:00:51 +0900

    init commit

---
+++ src/main/java/gwkim/batchperformance/BatchPerformanceApplication.java
@@ -0,0 +1,13 @@
+package gwkim.batchperformance;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BatchPerformanceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BatchPerformanceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/gwkim/batchperformance/BatchPerformanceApplicationTests.java
@@ -0,0 +1,13 @@
+package gwkim.batchperformance;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BatchPerformanceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
